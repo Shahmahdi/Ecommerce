ba61e96b08ac503e7a103e77ccee8b7f39d509e9
mahdi <shahmahdi11@gmail.com>
2019-07-13 01:03:48 +0600

    query product based on sortBy, order(asc/desc), limit

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -101,3 +101,24 @@ exports.update = (req, res) => {
     })
   })
 }
+
+exports.list = (req, res) => {
+
+  // products?sortBy=sold&order=desc&limit=4
+
+  let order = req.query.order ? req.query.order : 'asc';
+  let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+  let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+
+  Product.find()
+    .select('-photo')
+    .populate('category')
+    .sort([[sortBy, order]])
+    .limit(limit)
+    .exec((err, products) => {
+      if (err) {
+        res.status(400).json({ error: 'Product not found'});
+      }
+      res.send(products);
+    })
+};
